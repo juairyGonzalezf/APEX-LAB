1ced1e74476caaf7d769f8e432fa30e4811827c2
server21 <juairy.gonzalezf@outlook.com>
2026-04-30 18:35:04 +0200

    Auto-sync: 2026-04-30 18:35:04

--- import.sql
+++ import.sql
@@ -1,7 +1,8 @@
 set define off
 WHENEVER SQLERROR CONTINUE
 spool /home/tomcat/import/import_execution.log
-PROMPT --- Workspace TEST ya existe. Saltando creación.
+PROMPT --- Creando Workspace TEST
+@/home/tomcat/export/apex_240200/xepdb1/TEST/w5401601323772619.sql
 PROMPT --- Configurando Sesión para TEST
 begin
   apex_application_install.set_workspace('TEST');
@@ -36,7 +37,8 @@ PROMPT --- Importando Aplicaciones
 @/home/tomcat/export/apex_240200/xepdb1/TEST/f214.sql
 @/home/tomcat/export/apex_240200/xepdb1/TEST/f215.sql
 
-PROMPT --- Workspace TEST2 ya existe. Saltando creación.
+PROMPT --- Creando Workspace TEST2
+@/home/tomcat/export/apex_240200/xepdb1/TEST2/w7605142232319369.sql
 PROMPT --- Configurando Sesión para TEST2
 begin
   apex_application_install.set_workspace('TEST2');
@@ -55,7 +57,8 @@ PROMPT --- Importando Aplicaciones
 @/home/tomcat/export/apex_240200/xepdb1/TEST2/f100.sql
 @/home/tomcat/export/apex_240200/xepdb1/TEST2/f103.sql
 
-PROMPT --- Workspace TESTWORKSPACE ya existe. Saltando creación.
+PROMPT --- Creando Workspace TESTWORKSPACE
+@/home/tomcat/export/apex_240200/xepdb1/TESTWORKSPACE/w3200966620849279.sql
 PROMPT --- Configurando Sesión para TESTWORKSPACE
 begin
   apex_application_install.set_workspace('TESTWORKSPACE');
